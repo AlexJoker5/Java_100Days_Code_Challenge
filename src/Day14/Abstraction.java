14f3275f37aa06deb6493139424aa4dffd465041
Christopher Lone Toe <christikai2001@gmail.com>
2022-06-30 23:26:05 +0630

    This is 100Days Code Challenge of Java Basic From ground up.

--- src/Day14/Abstraction.java
+++
@@ -1,8 +0,0 @@
-package Day14;
-
-public class Abstraction {
-
-    /*Abstraction is the process of hiding certain details and showing only essential information to the user
-    * There are two ways to use abstraction : abstract class and interface.*/
-
-}
